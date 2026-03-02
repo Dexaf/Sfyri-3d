1da48ecb038b48e51b0225a2b362086640bdc0e8
Dexaf <federicopisajob@gmail.com>
2026-03-02 18:44:15 +0100

    FEAT: added vitest for testing

---
+++ vitest.setup.ts
@@ -0,0 +1,13 @@
+import { vi } from 'vitest';
+
+//NOTE: web gl context doesn't exists in test env, we mock it
+vi.mock('three', async () => {
+    const actual = await vi.importActual<typeof import('three')>('three');
+    return {
+        ...actual,
+        WebGLRenderer: class {
+            domElement = document.createElement('canvas');
+            setSize(width: number, height: number) { this.domElement.width = width; this.domElement.height = height; }
+        },
+    };
+});
